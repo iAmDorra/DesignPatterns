3529b45f8c51d08bce2415488e58c3585bac2b1a
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2018-02-25 21:16:03 +0100

    Initialize the first example Folder & File

---
+++ DesignPatternsTests/FolderFileLinkTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FolderFile;
+using NFluent;
+using System.Collections.Generic;
+
+namespace DesignPatterns
+{
+    [TestClass]
+    public class FolderFileLinkTest
+    {
+        [TestMethod]
+        public void Should_folder_contains_one_element_when_gettings_contains()
+        {
+            var textFile = new File { Name = "data.txt" };
+            var parentFolder = new Folder { Name = "Parent Folder" };
+            parentFolder.Files = new List<File> { textFile };
+
+            var containsNumber = parentFolder.GetContainsNumber();
+
+            Check.That(containsNumber).IsEqualTo(1);
+        }
+
+        [TestMethod]
+        public void Should_folder_contains_three_elements_when_gettings_contains()
+        {
+            var folder = CreateFolderTree();
+
+            var containsNumber = folder.GetContainsNumber();
+
+            Check.That(containsNumber).IsEqualTo(2);
+        }
+
+        private static Folder CreateFolderTree()
+        {
+            var textFile = new File { Name = "data.txt" };
+            var firstFolder = new Folder { Name = "Data Folder" };
+            var secondFolder = new Folder { Name = "Folder" };
+
+            var parentFolder = new Folder { Name = "Parent Folder" };
+            parentFolder.Files = new List<File> { textFile };
+            parentFolder.Folders = new List<Folder> { firstFolder };
+
+            return parentFolder;
+        }
+    }
+}
---
+++ DesignPatternsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DesignPatterns.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DesignPatterns.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e2058cb5-7668-4ce4-957b-e697a044f235")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
