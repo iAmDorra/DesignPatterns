87b15e01b60f5a2b09b1b58db241af9f9368a8fb
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2018-03-03 13:48:30 +0100

    Add ClientService project

---
+++ ClientService/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClientService
+{
+    public class Order
+    {
+        private List<Option> options = new List<Option>();
+
+        internal void AddNewOption(Option option)
+        {
+            this.options.Add(option);
+        }
+    }
+}
