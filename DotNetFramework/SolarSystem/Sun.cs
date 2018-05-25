04c4ec8e4377e6c73dc07a45c717d6c21c1ea3a1
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2018-05-25 13:31:17 +0200

    Correction of Sun exercice

--- DotNetFramework/SolarSystem/Sun.cs
+++ DotNetFramework/SolarSystem/Sun.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SolarSystem
+﻿namespace SolarSystem
 {
     public class Sun
     {
+        private static Sun instance = new Sun();
+
+        private Sun()
+        {
+        }
+
+        // The readonly property Instance can be a method too
+        //public static Sun GetInstance()
+        //{
+        //    return instance;
+        //}
+
+        public static Sun Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
     }
 }
