ce828b282b24709cabe178e4eb798dab60c68872
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2018-05-25 13:53:52 +0200

    Correction of Client service exercice

---
+++ DotNetFramework/ClientService/CustomerService.cs
@@ -0,0 +1,23 @@
+﻿namespace ClientService
+{
+    public class CustomerService
+    {
+        private readonly IOrdering ordering;
+        private readonly IBilling billing;
+
+        public CustomerService(IOrdering ordering, IBilling billing)
+        {
+            this.ordering = ordering;
+            this.billing = billing;
+        }
+
+        public void UpdateAdress(Client client, Adress newAdress)
+        {
+            ordering.UpdateAdress(client, newAdress);
+        }
+        public void AddOption(Order order, Option option)
+        {
+            billing.AddOption(order, option);
+        }
+    }
+}
