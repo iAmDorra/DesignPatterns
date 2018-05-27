52a5703067fd9f5f64354e612a0cf3a2276299c7
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2018-05-27 18:00:37 +0200

    Correction of Car dealer exercice

---
+++ DotNetFramework/CarDealer/FrenshOrder.cs
@@ -0,0 +1,22 @@
+﻿namespace CarDealer
+{
+    internal class FrenshOrder : BaseOrder
+    {
+        private double preTaxAmount;
+
+        public FrenshOrder(double preTaxAmount)
+        {
+            this.preTaxAmount = preTaxAmount;
+        }
+
+        protected override double CalculatePreTaxAmount()
+        {
+            return this.preTaxAmount * 0.196;
+        }
+
+        protected override double CalculateVat()
+        {
+            return this.preTaxAmount;
+        }
+    }
+}
---
+++ DotNetFramework/CarDealer/IOrder.cs
@@ -0,0 +1,7 @@
+﻿namespace CarDealer
+{
+    public interface IOrder
+    {
+        double CalculateAmount();
+    }
+}
