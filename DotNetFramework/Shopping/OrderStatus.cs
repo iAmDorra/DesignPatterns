3f115b423d21087f9274796ee558083c1cde811b
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2018-05-27 18:54:13 +0200

    Correction of shopping exercice

---
+++ DotNetFramework/Shopping/OrderStatus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping
+{
+    public abstract class OrderStatus
+    {
+        protected List<Item> items;
+
+        public OrderStatus(List<Item> items)
+        {
+            this.items = items;
+        }
+
+        public abstract void AddItem(Item item);
+        public abstract void RemoveItem(Item item);
+        public abstract OrderStatus NextStatus();
+    }
+}
