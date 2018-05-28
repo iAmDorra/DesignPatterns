61f219fb070f17486a6b2f9cfa91a63a6046f4f5
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2018-05-28 13:48:12 +0200

    Correction of SolarSystem exercice

--- DotNetCore/SolarSystem/Sun.cs
+++ DotNetCore/SolarSystem/Sun.cs
@@ -8,5 +8,24 @@ namespace SolarSystem
 {
     public class Sun
     {
+        private static Sun instance = new Sun();
+
+        private Sun()
+        {
+        }
+
+        // The readonly property Instance can be a method too
+        //public static Sun GetInstance()
+        //{
+        //    return instance;
+        //}
+
+        public static Sun Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
     }
 }
