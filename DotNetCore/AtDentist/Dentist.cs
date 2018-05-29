2710f41fd9bbb9685c536f84af47eb0d45274c02
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2018-05-29 12:37:08 +0200

    Correction of at the dentist exercice

---
+++ DotNetCore/AtDentist/Dentist.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtDentist
+{
+    public class Dentist
+    {
+        private IIterable patients;
+        // This property is used to count patients received by the dentist, it's not mandatory
+        private int numberPatients;
+        public int ReceivedPatients => numberPatients;
+
+        public Dentist(IIterable patientIterable)
+        {
+            this.numberPatients = 0;
+            this.patients = patientIterable;
+        }
+
+        public void EnterNext()
+        {
+            IIterator iterator = this.patients.GetIterator();
+            if (iterator.HasNext())
+            {
+                this.Receive(iterator.Next());
+            }
+        }
+
+        private void Receive(IElement element)
+        {
+            // This property is used to count patients received by the dentist
+            this.numberPatients++;
+            // Put here the behavior of receiving patient
+        }
+    }
+}
