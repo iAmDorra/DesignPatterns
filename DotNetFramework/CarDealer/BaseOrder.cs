8eb135907c0e2b1b9c4c9f5c0980425ea70c397b
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2019-04-14 19:42:36 +0200

    Correction of Car dealer exercice

---
+++ DotNetFramework/CarDealer/BaseOrder.cs
@@ -0,0 +1,13 @@
+﻿namespace CarDealer
+{
+    internal abstract class BaseOrder : IOrder
+    {
+        public double CalculateAmount()
+        {
+            return CalculateVat() + CalculatePreTaxAmount();
+        }
+
+        protected abstract double CalculateVat();
+        protected abstract double CalculatePreTaxAmount();
+    }
+}
---
+++ DotNetFramework/CarDealer/LuxembourgishOrder.cs
@@ -0,0 +1,25 @@
+﻿namespace CarDealer
+{
+    internal class LuxembourgishOrder : BaseOrder
+    {
+        private double preTaxServiceAmount;
+        private double preTaxMaterialAmount;
+
+        public LuxembourgishOrder(double preTaxServiceAmount, double preTaxMaterialAmount)
+        {
+            this.preTaxServiceAmount = preTaxServiceAmount;
+            this.preTaxMaterialAmount = preTaxMaterialAmount;
+        }
+
+        protected override double CalculatePreTaxAmount()
+        {
+            return this.preTaxServiceAmount * 0.12 +
+                this.preTaxMaterialAmount * 0.15;
+        }
+
+        protected override double CalculateVat()
+        {
+            return this.preTaxServiceAmount + this.preTaxMaterialAmount;
+        }
+    }
+}
